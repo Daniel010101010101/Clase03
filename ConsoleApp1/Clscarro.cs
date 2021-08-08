b5c052f4f37cad4878eb40568fbe024f8ce74660
Daniel010101010101 <88622971+Daniel010101010101@users.noreply.github.com>
2021-08-08 23:53:57 +0100

    Encapsulado

---
+++ ConsoleApp1/Clscarro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Clscarro
+    {
+
+        private String color;
+        private String marca;
+        private String modelo;
+        private int peso;
+        private int cantidad;
+
+        public string Color { get => color; set => color = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public int Peso { get => peso; set => peso = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+    }
+}
